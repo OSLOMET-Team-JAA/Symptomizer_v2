b5a882cf1420c896d4bce137c6e211091c52f775
Aleksander Korkh <korkh1979@gmail.com>
2022-11-28 00:34:28 +0100

    User class created

---
+++ Symptomizer_v2/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Symptomizer_v2.Models
+{
+    public class User
+    {
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
+        public String Username { get; set; }
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")]
+        public String Password { get; set; }
+    }
+}
